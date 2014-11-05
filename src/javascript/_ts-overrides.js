d1f53338cff6245d0608625591d5977717c18241
kcorkan <kcorkan@rallydev.com>
2014-11-05 14:34:30 -0800

    created grid ui

---
+++ src/javascript/_ts-overrides.js
@@ -0,0 +1,39 @@
+Ext.override(Ext.grid.column.Action, {
+
+	buttonText: 'myButton',
+    defaultRenderer: function(v, meta, record, rowIdx, colIdx, store, view){
+    	var me = this,
+            prefix = Ext.baseCSSPrefix,
+            scope = me.origScope || me,
+            items = me.items,
+            len = items.length,
+            i = 0,
+            item, ret, disabled, tooltip;
+ 
+        // Allow a configured renderer to create initial value (And set the other values in the "metadata" argument!)
+        // Assign a new variable here, since if we modify "v" it will also modify the arguments collection, meaning
+        // we will pass an incorrect value to getClass/getTip
+        ret = Ext.isFunction(me.origRenderer) ? me.origRenderer.apply(scope, arguments) || '' : '';
+
+        meta.tdCls += ' ' + Ext.baseCSSPrefix + 'action-col-cell';
+        for (; i < len; i++) {
+            item = items[i];
+
+            disabled = item.disabled || (item.isDisabled ? item.isDisabled.call(item.scope || scope, view, rowIdx, colIdx, item, record) : false);
+            tooltip = disabled ? null : (item.tooltip || (item.getTip ? item.getTip.apply(item.scope || scope, arguments) : null));
+
+            // Only process the item action setup once.
+            if (!item.hasActionConfiguration) {
+
+                // Apply our documented default to all items
+                item.stopSelection = me.stopSelection;
+                item.disable = Ext.Function.bind(me.disableAction, me, [i], 0);
+                item.enable = Ext.Function.bind(me.enableAction, me, [i], 0);
+                item.hasActionConfiguration = true;
+            }
+
+       ret += '<button class="' + me.buttonCls + ' ' + prefix + 'action-col-' + String(i) + '">' + me.buttonText + '</button>';     
+        }
+        return ret;    
+    },
+});
--- src/javascript/app.js
+++ src/javascript/app.js
@@ -3,47 +3,91 @@ Ext.define('CustomApp', {
     componentCls: 'app',
     logger: new Rally.technicalservices.Logger(),
     items: [
-        {xtype:'container',itemId:'message_box',tpl:'Hello, <tpl>{_refObjectName}</tpl>'},
-        {xtype:'container',itemId:'display_box'},
+        {xtype:'container',itemId:'button_box', padding: 10},
+        {xtype:'container',itemId:'grid_box'},
         {xtype:'tsinfolink'}
     ],
     launch: function() {
-        this.down('#message_box').update(this.getContext().getUser());
-        
-        var m_name = 'Defect',
-        m_fields = ['Name','State'];
-        
-        this._loadAStoreWithAPromise(m_name, m_fields).then({
-            scope: this,
-            success: function(store){
-                this.down('#display_box').add({
-                    xtype: 'rallygrid',
-                    store: store,
-                    columnCfgs: m_fields
-                });
-            },
-            failure: function(error_message){
-                alert(error_message);
-            }
-        });
+
+    	this.down('#button_box').add({
+    		xtype: 'rallybutton',
+    		text: '+Add New',
+    		scope: this,
+    		handler: this._addNewProcess
+    	});
+    	
+    	this.down('#grid_box').add({
+    		xtype: 'rallygrid',
+    		store: this._fetchProcessStore(),
+    		columnCfgs: this._getProcessGridColumnCfgs()
+    	});
     },
-    _loadAStoreWithAPromise: function(model_name, model_fields){
-        var deferred = Ext.create('Deft.Deferred');
-        
-        var defectStore = Ext.create('Rally.data.wsapi.Store', {
-            model: model_name,
-            fetch: model_fields,
-            autoLoad: true,
-            listeners: {
-                load: function(store, records, successful) {
-                    if (successful){
-                        deferred.resolve(store);
-                    } else {
-                        deferred.reject('Failed to load store for model [' + model_name + '] and fields [' + model_fields.join(',') + ']');
-                    }
+    _addNewProcess: function(){
+    	this.logger.log('_addNewProcess');
+    },
+    _fetchProcessStore: function(){
+    	this.logger.log('_fetchProcessStore');
+    	var store = Ext.create('Rally.data.custom.Store', {
+    	        data: [{
+    	        	'Name': 'User Story Blocked Process',
+    	        	'ShortName' : 'Block',
+    	        	'Type' : 'UserStory',
+    	        	'Field':'Blocked'
+    	        },{
+    	        	'Name': 'Add New Feature',
+    	        	'ShortName' : 'AddNew',
+    	        	'Type' : 'PortfolioItem/Feature',
+    	        	'Field':''
+    	        }]
+    	    });
+    	return store; 
+    },
+    _getProcessGridColumnCfgs: function(){
+    	this.logger.log('_getProcessGridColumnCfgs');
+    	var me = this;
+    	
+      	var columns = [{ 
+            text: 'Name',
+            dataIndex: 'Name',
+            width: 200,
+        },{
+            text: 'Short Name',
+            dataIndex: 'ShortName',
+        },{
+        	text: 'Type',
+        	dataIndex: 'Type'
+        },{
+        	text: 'Field',
+        	dataIndex: 'Field'
+        },{
+			xtype: 'actioncolumn',
+			buttonText: 'Edit',
+			buttonCls: 'ts-secondary-button',
+			scope: this,
+			items: [{
+				scope: this,
+                handler: function(grid, row, col) {
+                	me._editProcess(grid, row);
                 }
-            }
-        });
-        return deferred.promise;
+            }]
+		},{
+			xtype: 'actioncolumn',
+			buttonText: 'Delete',
+			buttonCls: 'ts-secondary-button',
+			scope: this,
+			items: [{
+				scope: this,
+                handler: function(grid, row, col) {
+                	me._deleteProcess(grid, row);
+                }
+            }]
+		}]; 
+      	return columns; 
+    },
+    _editProcess: function(grid, row){
+    	this.logger.log('_editProcess',grid,row);
+    },
+    _deleteProcess: function(grid, row){
+    	this.logger.log('_deleteProcess',grid,row);
     }
 });
